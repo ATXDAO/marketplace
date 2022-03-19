417f62768aef37b4ab8ffdb02ebe917b843faa4b
Neil Kistner <neil.kistner@gmail.com>
2022-03-19 13:37:28 -0500

    Fix expired listings not showing in inventory

--- src/graphql/marketplace.graphql.ts
+++ src/graphql/marketplace.graphql.ts
@@ -3,7 +3,7 @@ import gql from "graphql-tag";
 export const getUserInventory = gql`
   query getUserInventory($id: ID!) {
     user(id: $id) {
-      listings(where: { status: Active }) {
+      listings(where: { status_in: [Active, Expired] }) {
         id
         expires
         pricePerItem
